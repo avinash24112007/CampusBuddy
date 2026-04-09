a733d8adad480f6e51deb67d0010727ecfca2c4e
avinash24112007 <avinash24112007@gmail.com>
2026-04-10 02:08:35 +0530

    Remove: Removed redundant tables

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from typing import AsyncGenerator
 from contextlib import asynccontextmanager
 from database import Base, engine
-from models import caffenity_models, credentials_models, arena_models, problem_box_models, eShoppers_models
+from models import caffenity_models, credentials_models, arena_models
 
 
 
