614bdf87b826583fae58f9d68ce8253de7f5da50
avinash24112007 <avinash24112007@gmail.com>
2026-04-10 06:10:27 +0530

    feat: implement arena event management and user authentication modules

--- models/problembox_models.py
+++ models/problembox_models.py
@@ -1,8 +1,9 @@
 import uuid
+import random
 from datetime import datetime
 import enum
 from sqlalchemy import String, Text, Boolean, Integer, DateTime, ForeignKey, text, Enum
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID, ARRAY
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database import Base
 
@@ -18,10 +19,13 @@ class TicketStatus(str, enum.Enum):
     InProgress = 'InProgress'
     Resolved = 'Resolved'
 
+def generate_tkt_id():
+    return f"TKT-{random.randint(1000, 9999)}"
+
 class Ticket(Base):
     __tablename__ = 'tickets'
 
-    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()"))
+    id: Mapped[str] = mapped_column(String(20), primary_key=True, default=generate_tkt_id)
     title: Mapped[str] = mapped_column(String(255), nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=False)
     location: Mapped[str] = mapped_column(String(255), nullable=True)
@@ -31,7 +35,13 @@ class Ticket(Base):
     status: Mapped[TicketStatus] = mapped_column(Enum(TicketStatus), default=TicketStatus.Raised)
     
     reporter_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    reporter = relationship("User", foreign_keys=[reporter_id])
     is_anonymous: Mapped[bool] = mapped_column(Boolean, default=False)
+    
+    upvotes: Mapped[int] = mapped_column(Integer, default=0)
+    has_resolved_viewed: Mapped[bool] = mapped_column(Boolean, default=False)
+    media: Mapped[list[str]] = mapped_column(ARRAY(Text), default=list)
+
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=text('now()'))
 
     timeline = relationship("TicketTimeline", backref="ticket", cascade="all, delete-orphan")
@@ -39,8 +49,8 @@ class Ticket(Base):
 class TicketTimeline(Base):
     __tablename__ = 'ticket_timeline'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    ticket_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('tickets.id', ondelete='CASCADE'), nullable=False)
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()"))
+    ticket_id: Mapped[str] = mapped_column(ForeignKey('tickets.id', ondelete='CASCADE'), nullable=False)
     step_name: Mapped[str] = mapped_column(String(100), nullable=False)
     update_text: Mapped[str] = mapped_column(Text, nullable=True)
     is_completed: Mapped[bool] = mapped_column(Boolean, default=False)
