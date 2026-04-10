14ef49405c0c08bf04daeda4a011edf343bfbef4
avinash24112007 <avinash24112007@gmail.com>
2026-04-10 06:10:10 +0530

    feat: implement arena and problembox modules with associated models, schemas, and routes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from typing import AsyncGenerator
 from contextlib import asynccontextmanager
 from database import Base, engine
 from models import caffenity_models, credentials_models, arena_models, problembox_models, user_models, shopperz_models
-from routes import uassist_routes, auth_routes, caffenity_routes, shopperz_routes
+from routes import uassist_routes, auth_routes, caffenity_routes, shopperz_routes, problembox_routes, user_routes, arena_routes
 
 
 
@@ -27,6 +27,9 @@ app.include_router(auth_routes.router)
 app.include_router(uassist_routes.router)
 app.include_router(caffenity_routes.router)
 app.include_router(shopperz_routes.router)
+app.include_router(problembox_routes.router)
+app.include_router(user_routes.router)
+app.include_router(arena_routes.router)
 
 
 @app.get('/')
